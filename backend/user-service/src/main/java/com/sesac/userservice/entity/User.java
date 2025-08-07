da05c825160ddc27d840302cf3684715c9920839
calendar2 <zoro94317@gmail.com>
2025-08-07 14:42:13 +0900

    feat: user-service 프로젝트 초기 구축 - 사용자 단일조회 - 3티어 구조 생성

---
+++ backend/user-service/src/main/java/com/sesac/userservice/entity/User.java
@@ -0,0 +1,45 @@
+package com.sesac.userservice.entity;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(unique = true, nullable = false, length = 50)
+	private String email;
+
+	@Column(nullable = false)
+	private String password;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	@CreationTimestamp
+	@Column(name = "created_at", updatable = false)
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+}
---
+++ backend/user-service/src/main/java/com/sesac/userservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sesac.userservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sesac.userservice.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
